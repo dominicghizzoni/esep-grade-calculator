6de30c140a8a0e1223d093288a68559c91108f5a
Dominic Ghizzoni <dominicghizzoni@ufl.edu>
2025-10-02 21:09:12 -0400

    Created unit tests for new pass/fail requirements

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -112,3 +112,127 @@ func TestGradeTypeString(t *testing.T) {
 		})
 	}
 }
+
+func TestGetPassForGradeA(t *testing.T) {
+	expected_value := "Pass"
+
+	gradeCalculator := NewGradeCalculatorWithPassFail(true)
+
+	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
+	gradeCalculator.AddGrade("exam 1", 100, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 100, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetFinalGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetPassForGradeB(t *testing.T) {
+	expected_value := "Pass"
+
+	gradeCalculator := NewGradeCalculatorWithPassFail(true)
+
+	gradeCalculator.AddGrade("open source assignment", 80, Assignment)
+	gradeCalculator.AddGrade("exam 1", 81, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 85, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetFinalGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetPassForGradeC(t *testing.T) {
+	expected_value := "Pass"
+
+	gradeCalculator := NewGradeCalculatorWithPassFail(true)
+
+	gradeCalculator.AddGrade("open source assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 1", 71, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 75, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetFinalGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetFailForGradeD(t *testing.T) {
+	expected_value := "Fail"
+
+	gradeCalculator := NewGradeCalculatorWithPassFail(true)
+
+	gradeCalculator.AddGrade("open source assignment", 60, Assignment)
+	gradeCalculator.AddGrade("exam 1", 61, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 65, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetFinalGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetFailForGradeF(t *testing.T) {
+	expected_value := "Fail"
+
+	gradeCalculator := NewGradeCalculatorWithPassFail(true)
+
+	gradeCalculator.AddGrade("open source assignment", 50, Assignment)
+	gradeCalculator.AddGrade("exam 1", 51, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 55, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetFinalGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetPassForBoundaryC(t *testing.T) {
+	expected_value := "Pass"
+
+	gradeCalculator := NewGradeCalculatorWithPassFail(true)
+
+	gradeCalculator.AddGrade("open source assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 1", 70, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 70, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetFinalGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetFailForBoundaryD(t *testing.T) {
+	expected_value := "Fail"
+
+	gradeCalculator := NewGradeCalculatorWithPassFail(true)
+
+	gradeCalculator.AddGrade("open source assignment", 69, Assignment)
+	gradeCalculator.AddGrade("exam 1", 69, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 69, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetFinalGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestEmptyCategoriesPassFail(t *testing.T) {
+	expected_value := "Fail"
+
+	gradeCalculator := NewGradeCalculatorWithPassFail(true)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetFinalGrade with empty categories to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
